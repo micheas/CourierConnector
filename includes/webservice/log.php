976e191fd8470e7a1f7f8935d309efd056ed20ac
DevSys <DevSys@DevSys-Laptop>
2013-05-27 22:53:15 -0700

    reformatted the layout bringing to current structure. added new mapping and request parameters into json

--- includes/webservice/log.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function log_transaction($type, $status, $message)
-{
-    db_query("insert into courier_transaction_log (trans_date, type, message, status) value('%s', '%s', '%s', '%s')",
-            array(date('Y-m-d h:m:s'), $type, $message, $status));
-}
-
-?>
